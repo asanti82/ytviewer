63e4e45cc59aefa1fba0a28e816a4def6e029ec3
Andres Santillan <andre.santillan@gmail.com>
2015-02-18 10:06:58 -0500

    first upload of entire project

---
+++ viewer.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace yviewer
+{
+    partial class viewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(viewer));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtVideoUrl = new System.Windows.Forms.TextBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Youtube video url";
+            // 
+            // txtVideoUrl
+            // 
+            this.txtVideoUrl.Location = new System.Drawing.Point(14, 23);
+            this.txtVideoUrl.Name = "txtVideoUrl";
+            this.txtVideoUrl.Size = new System.Drawing.Size(225, 20);
+            this.txtVideoUrl.TabIndex = 1;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(254, 20);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 2;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(0, 49, 0, 0);
+            this.panel1.Size = new System.Drawing.Size(371, 264);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.btnLoad);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.txtVideoUrl);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(375, 49);
+            this.panel2.TabIndex = 4;
+            // 
+            // viewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 264);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "viewer";
+            this.Text = "asantillan.com - youtube player";
+            this.TopMost = true;
+            this.Resize += new System.EventHandler(this.viewer_Resize);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtVideoUrl;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
---
+++ viewer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yviewer
+{
+    public partial class viewer : Form
+    {
+        
+        WebBrowser wbYoutube = new WebBrowser();
+
+        public viewer()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            if (txtVideoUrl.Text != string.Empty)
+            {
+
+                string url = txtVideoUrl.Text;
+                string id = Regex.Match(url, @"^(?:https?\:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v\=))([\w-]{10,12})(?:$|\&|\?\#).*").Groups[1].Value;
+
+                if (id != string.Empty)
+                {
+                    wbYoutube.Url = new Uri("http://www.youtube.com/embed/" + id + "?autoplay=1&hl=en&fs=1");
+                    panel1.Controls.Add(wbYoutube);
+                    wbYoutube.Dock = DockStyle.Fill;
+                }
+                else
+                {
+                    MessageBox.Show("Please insert a valid youtube url", "Invalid Youtube URL", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                
+
+            }
+        }
+
+        private void viewer_Resize(object sender, EventArgs e)
+        {
+            panel1.AutoSize = true;
+            wbYoutube.Size = new System.Drawing.Size(this.Size.Width, this.Size.Height);
+            
+        }
+
+
+    }
+}
